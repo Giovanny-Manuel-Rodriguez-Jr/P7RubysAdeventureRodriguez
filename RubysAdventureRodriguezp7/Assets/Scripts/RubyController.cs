3badf5a0272943e8b7e66ef67daf30e4fc84fe71
Giovanny Rodriguez Jr <751764@student.dusd.net>
2023-10-27 15:25:49 -0700

    Added a new script.
    I got ruby to move horzontally and verticaly

--- RubysAdventureRodriguezp7/Assets/Scripts/RubyController.cs
+++ RubysAdventureRodriguezp7/Assets/Scripts/RubyController.cs
@@ -7,12 +7,20 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 postiton = transform.position;
+        postiton.x = postiton.x + 0.1f * horizontal;
+        postiton.y = postiton.y + 0.01f * vertical;
+
+        transform.position = postiton;
     }
 }
